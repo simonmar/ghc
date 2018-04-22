1eb9cea74a7ee6662057872a413cfb2b2ca2a824
Simon Marlow <marlowsd@gmail.com>
2018-04-22 14:28:47 +0100

    Save a word in the info table on x86_64
    Summary:
    An info table with an SRT normally looks like this:
    
        StgWord64 srt_offset
        StgClosureInfo layout
        StgWord32 layout
        StgWord32 has_srt
    
    But we only need 32 bits for srt_offset on x86_64, because the small
    memory model requires that code segments are at most 2GB. So we can
    optimise this to
    
        StgClosureInfo layout
        StgWord32 layout
        StgWord32 srt_offset
    
    saving a word.  We can tell whether the info table has an SRT or not,
    because zero is not a valid srt_offset, so zero still indicates that
    there's no SRT.
    
    Test Plan:
    * validate
    * For results, see D4632.
    
    Reviewers: bgamari, niteria, osa1, erikd
    
    Subscribers: thomie, carter
    
    Differential Revision: https://phabricator.haskell.org/D4634

--- compiler/cmm/CmmBuildInfoTables.hs
+++ compiler/cmm/CmmBuildInfoTables.hs
@@ -100,8 +100,12 @@ In each case, the info table refers to the SRT.
 
 info->srt is non-zero if there's an SRT
 info->f.srt_offset refers to the SRT
-(but see TODO below, we can improve this)
 
+On x86_64, we optimise the info table representation further.  The
+offset to the SRT can be stored in 32 bits (all code lives within a
+2GB region in x86_64's small memory model), so we can save a word in
+the info table by storing the srt_offset in the srt field, which is
+half a word.
 
 EXAMPLE
 ^^^^^^^
@@ -191,9 +195,6 @@ implemented.
 
 As an alternative to [FUN]: we could merge the FUN's SRT with the FUN
 object itself.
-
-TODO: make info->srt be an offset to the SRT, or zero if none (save
-one word per info table that has an SRT)
 -}
 
 -- ---------------------------------------------------------------------
